6bb9ecc8bb1d9e5475dd8ebe592ee6862798ccd3
Arseny <iv.ars@mail.ru>
2021-08-29 15:00:42 +0300

    AtomicGrid_MISiS_HACK

---
+++ index.js
@@ -0,0 +1,84 @@
+ymaps.ready(init);
+
+function init() {
+    var myPlacemark,
+        myMap = new ymaps.Map('map', {
+            center: [46.951171008178356, 142.73699579414946],
+            zoom: 18
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+    // Слушаем клик на карте.
+    myMap.events.add('click', function (e) {
+        var coords = e.get('coords');
+
+        // Если метка уже создана – просто передвигаем ее.
+        if (myPlacemark) {
+            myPlacemark.geometry.setCoordinates(coords);
+        }
+        // Если нет – создаем.
+        else {
+            myPlacemark = createPlacemark(coords);
+            myMap.geoObjects.add(myPlacemark);
+            // Слушаем событие окончания перетаскивания на метке.
+            myPlacemark.events.add('dragend', function () {
+                getAddress(myPlacemark.geometry.getCoordinates());
+            });
+        }
+        getAddress(coords);
+    });
+
+    // Создание метки.
+    function createPlacemark(coords) {
+        return new ymaps.Placemark(coords, {
+            iconCaption: 'поиск...'
+        }, {
+            preset: 'islands#violetDotIconWithCaption',
+            draggable: true
+        });
+    }
+
+    // Определяем адрес по координатам (обратное геокодирование).
+    function getAddress(coords) {
+        myPlacemark.properties.set('iconCaption', 'поиск...');
+        ymaps.geocode(coords).then(function (res) {
+            var firstGeoObject = res.geoObjects.get(0);
+            document.getElementById("address").innerHTML = firstGeoObject.getAddressLine();
+            document.getElementById("address_2").innerHTML = firstGeoObject.getAddressLine();
+            console.log(firstGeoObject.getAddressLine());
+
+            myPlacemark.properties
+                .set({
+                    // Формируем строку с данными об объекте.
+                    iconCaption: [
+                        // Название населенного пункта или вышестоящее административно-территориальное образование.
+                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+                        firstGeoObject.getAddressLine()
+                    ].filter(Boolean).join(', '),
+                    // В качестве контента балуна задаем строку с адресом объекта.
+                    balloonContent: firstGeoObject.getAddressLine()
+                });
+        });
+    }
+}
+
+
+function Click(){
+    let a = document.getElementById("first");
+    let b = document.getElementById("second");
+    console.log(a);
+    console.log(b);
+    a.style.display = "none";
+    b.style.display = "block";
+}
+
+function Click1(){
+    let a = document.getElementById("first");
+    let b = document.getElementById("second");
+    a.style.display = "block";
+    b.style.display = "none";
+}
+
+
